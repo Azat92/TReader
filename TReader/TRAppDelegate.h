4220d93deeeb1acdd022af49f17f68c3e9450989
Azat Almeev <azat@iMac-Azat.local>
2013-09-06 20:28:26 +0400

    Initial Commit

---
+++ TReader/TRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TRAppDelegate.h
+//  TReader
+//
+//  Created by Azat Almeev on 06.09.13.
+//  Copyright (c) 2013 Azat Almeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TReader/TRViewController.h
@@ -0,0 +1,13 @@
+//
+//  TRViewController.h
+//  TReader
+//
+//  Created by Azat Almeev on 06.09.13.
+//  Copyright (c) 2013 Azat Almeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRViewController : UIViewController
+
+@end
